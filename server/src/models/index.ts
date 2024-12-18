151f0c39145a3e4244d6df7d26804679bb2e6f10
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-17 21:10:41 -0600

    updated models/index.ts, package.json and .env

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -9,13 +9,18 @@ console.log('Database Name:', process.env.DB_NAME);
 console.log('Database User:', process.env.DB_USER);
 console.log('Database Password:', process.env.DB_PASSWORD);
 
-const sequelize = new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-    host: 'localhost',
-    dialect: 'postgres',
-    dialectOptions: {
-        decimalNumbers: true,
-    },
-});
+const sequelize = process.env.DB_URL 
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true, 
+          rejectUnauthorized: false,
+        },
+      },
+    });
 
 // Test the database connection
 sequelize.authenticate()
@@ -33,3 +38,7 @@ User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
 Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
 
 export { sequelize, User, Ticket };
+
+// dialectOptions: {
+//     decimalNumbers: true,
+//   },
