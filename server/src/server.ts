322faec8f15f0784bc034546fc070a6db62715f5
5mitty <43888178+5mitty@users.noreply.github.com>
2024-12-17 22:28:19 -0600

    JWT Auth working with pg db!!!

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import cors from 'cors';
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+app.use(cors());
 app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
